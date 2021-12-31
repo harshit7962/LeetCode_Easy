d60d3c7f3606d402d4cb13cd925bb547d9247f45
Harshit <62823486+harshit7962@users.noreply.github.com>
2021-12-31 14:11:56 +0530

    Check if given string of parenthesis is valid
    runtime: 0ms
    memory usage: 6.4 MB

---
+++ Valid_Paranthesis.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2==1) return false;
+        stack<char> st;
+        for(int i=0;i<s.length();i++) {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else if(st.empty()) return false;
+            else if((st.top()=='{'&&s[i]=='}') || (st.top()=='('&&s[i]==')') || (st.top()=='['&&s[i]==']'))
+                st.pop();
+            else return false;
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
